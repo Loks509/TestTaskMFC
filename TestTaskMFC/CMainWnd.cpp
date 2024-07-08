ccf5d8dbab179329ace4b859e5019a157189cbf9
Loks509 <kow20002204@mail.ru>
2024-07-08 11:16:34 +0300

    добавлен статус бар

--- TestTaskMFC/CMainWnd.cpp
+++ TestTaskMFC/CMainWnd.cpp
@@ -1,7 +1,22 @@
 #include "CMainWnd.h"
+BEGIN_MESSAGE_MAP(CMainWnd, CFrameWnd)
+	ON_WM_CREATE()
+END_MESSAGE_MAP()
 
 CMainWnd::CMainWnd()
 {
 	Create(NULL, "Hello world!", WS_OVERLAPPEDWINDOW, rectDefault,
 		NULL, NULL);
+}
+
+int CMainWnd::OnCreate(LPCREATESTRUCT lpCreateStruct) {
+	if (CFrameWnd::OnCreate(lpCreateStruct) == -1) {
+		return -1;
+	}
+	m_wndStatusBar.Create(this);
+	return 0;
+}
+
+CMainWnd::~CMainWnd()
+{
 }
--- TestTaskMFC/CMainWnd.h
+++ TestTaskMFC/CMainWnd.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <afxwin.h>
+#include <afxext.h>
+
 class CMainWnd :
     public CFrameWnd
 {
 public:
     CMainWnd();
+    int OnCreate(LPCREATESTRUCT lpCreateStruct);
+    ~CMainWnd();
+private:
+    CStatusBar m_wndStatusBar;
+    DECLARE_MESSAGE_MAP();
 };
+
+
 
